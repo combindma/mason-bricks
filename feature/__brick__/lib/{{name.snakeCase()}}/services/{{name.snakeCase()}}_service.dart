6ebaa29656836a62fb846a7cdad80a4312a84946
Combind <webmaster@combind.ma>
2026-01-16 23:21:48 +0100

    Added feature brick

---
+++ feature/__brick__/lib/{{name.snakeCase()}}/services/{{name.snakeCase()}}_service.dart
@@ -0,0 +1,5 @@
+/*import 'core/services/base_service.dart';
+
+class {{name.pascalCase()}}Service extends BaseService {
+  {{name.pascalCase()}}Service(super.ref);
+}*/
---
+++ feature/__brick__/lib/{{name.snakeCase()}}/{{name.snakeCase()}}_provider.dart
@@ -0,0 +1,5 @@
+/*
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final {{name.pascalCase()}}ServiceProvider = Provider<{{name.pascalCase()}}Service>((ref) => {{name.pascalCase()}}Service(ref));
+*/
