1b9f26dd04db4c230b0f932060b1b11aeb14cb36
Combind <webmaster@combind.ma>
2026-07-18 19:39:50 +0100

    wip

--- starter/__brick__/lib/bootstrap/providers.dart
+++ starter/__brick__/lib/bootstrap/providers.dart
@@ -1,5 +1,6 @@
 export 'storage_provider.dart';
 export 'api_provider.dart';
+export 'cms_api_provider.dart';
 export 'theme_provider.dart';
 export 'error_provider.dart';
 export 'remote_config_provider.dart';
--- starter/__brick__/lib/core/services/service.dart
+++ starter/__brick__/lib/core/services/service.dart
@@ -1,11 +1,18 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../bootstrap/providers.dart';
+import 'api_service.dart';
+import 'cms_api_service.dart';
+
 
 abstract class Service {
   Service(this._ref);
   final Ref _ref;
 
-  //ApiService get _api => ref.read(apiServiceProvider);
+  ApiService get api => _ref.read(apiServiceProvider);
+  CmsApiService get cms => _ref.read(cmsApiServiceProvider);
   StorageService get storage => _ref.read(storageServiceProvider);
+
+  /// Safe: `remoteConfigProvider` is eagerly initialized.
+  RemoteConfigService get config => _ref.read(remoteConfigProvider).requireValue;
 }
